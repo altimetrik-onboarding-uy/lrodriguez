133ad1b205f13814a03708bc3e511f8a6a18b0b6
Leandro Rodriguez <lrodriguez@altimetrik.com>
2018-04-23 17:58:18 -0300

    part 2 almost done

--- src/aura/Compensation_List_Component/Compensation_List_ComponentController.js
+++ src/aura/Compensation_List_Component/Compensation_List_ComponentController.js
@@ -1,6 +1,28 @@
 ({
 	doInit : function(component, event, helper) {
 		var action = component.get("c.getCompensations");
+		var comp_type = 'all';
+		action.setParams({
+            "filter": comp_type
+        });
+		action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.compensations", response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        // Send action off to be executed
+        $A.enqueueAction(action);
+	},
+	applyFilter : function(component, event, helper) {
+		var filterOption = event.getSource().get('v.name');
+		var action = component.get("c.getCompensations");
+		action.setParams({
+            "filter": filterOption
+        });
 		action.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
--- src/aura/compensationItem/compensationItemController.js
+++ src/aura/compensationItem/compensationItemController.js
@@ -1,4 +1,17 @@
 ({
-	myAction : function(component, event, helper) {
+	doInit : function(component, event, helper) {
+		var comp = component.get("v.compensation");
+		var date_of_birth = comp.Employee__r.Birthdate;
+		if (date_of_birth != undefined){
+			component.set("v.birthdate", date_of_birth);
+		}
+	},
+	editRecord : function(component, event, helper) {
+		var comp = component.get("v.compensation");
+	    var editRecordEvent = $A.get("e.force:editRecord");
+	    editRecordEvent.setParams({
+	         "recordId": comp.Employee__r.Id
+	    });
+	    editRecordEvent.fire();
 	}
 })
